747e97064de3d6ab60c843848c563507e697aa2d
fic <fic@NOTE02-S28>
2025-02-14 21:57:22 -0300

    Adicionar arquivos de projeto.

---
+++ Celular.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoCelular
+{
+    public class Celular
+    {
+        public string Cor;
+        public string Modelo;
+        public string Tamanho;
+        public bool Ligado;
+
+        public void Ligar()
+        {
+            Console.WriteLine("Ligando o Celular...");
+            Ligado= true;
+        }
+        public void Desligar()
+        {
+            Console.WriteLine("Celular Desligado");
+            Ligado= false; 
+        }
+   
+        public void BotaoFazerLigacao()
+        {
+            if (Ligado)
+            {
+                Console.WriteLine("Chamando...");
+            }
+            else
+            {
+                Console.WriteLine("Celular Desligado");
+            }
+        }
+        public void BotaoEnviarMensagem()
+        {
+            Console.WriteLine("Enviando SMS...");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// Criem uma Classe Celular
+// - Cor, Modelo, Tamanho, Ligado ou Nao (bool)
+// - Metodos - ligar,desligar, fazer ligacao, enviar mensagem
+// - Os metodos so podem ser executados se o celular estiver ligado 
+
+using ProjetoCelular;
+
+Celular Caracter = new Celular();
+
+Caracter.Cor = "Branco";
+Caracter.Modelo = "iPhone";
+Caracter.Tamanho = "11cm";
+Caracter.Power = true;
+
+Caracter.BotaoPower();
